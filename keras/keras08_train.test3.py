5437667066cf282d09224f0ef22690299d2be809
rlaxoghd0513 <rlaxoghd0513@naver.com>
2023-03-02 18:30:52 +0900

    third commit

--- keras/keras08_train.test3.py
+++ keras/keras08_train.test3.py
@@ -20,13 +20,15 @@ model=Sequential()
 model.add(Dense(1, input_dim=1))
 model.add(Dense(3))
 model.add(Dense(5))
+model.add(Dense(6))
+model.add(Dense(5))
 model.add(Dense(4))
 model.add(Dense(2))
 model.add(Dense(1))
 
 #컴파일 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=100, batch_size=10)
+model.fit(x_train, y_train, epochs=200, batch_size=10)
 
 #평가 예측
 loss=model.evaluate(x_test, y_test)
