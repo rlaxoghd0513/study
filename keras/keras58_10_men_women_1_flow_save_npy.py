41b59a209f8b8a4ab8c238a8cf3e458a6141a7d1
rlaxoghd0513 <rlaxoghd0513@naver.com>
2023-04-03 22:19:40 +0900

    commit

--- keras/keras58_10_men_women_1_flow_save_npy.py
+++ keras/keras58_10_men_women_1_flow_save_npy.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np#2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
