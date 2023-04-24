dc70a22bb949b6fe9edb6f28ac72d0c72b8589b0
rlaxoghd0513 <rlaxoghd0513@naver.com>
2023-04-24 22:44:06 +0900

    commit

--- ml/m33_pca_mnist2_DNN.py
+++ ml/m33_pca_mnist2_DNN.py
@@ -7,31 +7,49 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, Dropout
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score
+from tensorflow.keras.utils import to_categorical
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()   #y가져오기 싫다 파이썬 기초문법 _작대기
 
-x = np.append(x_train, x_test, axis=0)
-print(x.shape) 
-y = np.append(y_train, y_test, axis=0)
+# x = np.append(x_train, x_test, axis=0)
+# print(x.shape) 
+# y = np.append(y_train, y_test, axis=0)
 
-x = x.reshape(70000,784)
+# x = x.reshape(70000,784)
+# y = to_categorical(y)
 n_components_list = [154,331,486,713]
 list_name = ['0.95','0.99','0.999','1.0']
 
-print('나의 최고의 CNN: 0.9814')
-print('나의 최고의 DNN: 0.9223')
-
 for i,value in enumerate(n_components_list):
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()   #y가져오기 싫다 파이썬 기초문법 _작대기
+
+    x = np.append(x_train, x_test, axis=0)
+    print(x.shape) 
+    y = np.append(y_train, y_test, axis=0)
+
+    x = x.reshape(70000,784)
+    y = to_categorical(y)
+    
     pca = PCA(n_components = value)
     x = pca.fit_transform(x)
     
     x_train, x_test, y_train, y_test = train_test_split(x,y, train_size = 0.8, random_state=123, shuffle=True)
 
-    model = RandomForestClassifier(random_state=123) #훈련할때마다 바뀌니까 random_state고정
+    model = Sequential() 
+    model.add(Dense(16, input_dim=n_components_list[i]))
+    model.add(Dense(10,activation='softmax'))
+    model.compile(loss= 'categorical_crossentropy', optimizer = 'adam', metrics=['acc'])
+    model.fit(x_train, y_train, epochs=1, batch_size=32, validation_split = 0.2)
+    
+    acc = model.evaluate(x_test, y_test)[1]
+    print('pca',list_name[i],':', acc)
 
-    model.fit(x_train, y_train)
-    results = model.score(x_test, y_test)
-    print('pca',list_name[i],':', results)
-    pca = PCA(n_components = 784)
+
+# 최고DNN : 
+# pca 0.95 : 4.275590896606445
+# pca 0.99 : 4.512420177459717
+# pca 0.999 : 3.5811731815338135
+# pca 1.0 : 3.6105496883392334
+
 
 
